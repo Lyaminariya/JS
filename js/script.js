ffceb545a39a3e07b9045efa52c9b4953583dd67
Sergey <lyamkin.serezha@mail.ru>
2022-03-01 13:00:25 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+console.log("arr" + " - object");
+console.log(4 +"5");
+
+let incr = 10,
+    decr = 10;
+
+console.log(++incr, --decr);
+
+console.log(2  + 2 * 2 !== '');
+
+const isChecked = false,
+        isClose = false;
+
+console.log(isChecked || !isClose);
